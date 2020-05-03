4bf0f9debdf8e52539f21b9a3d0aaec12246c331
manisha16-mahi <manishakrigupta16899@gmail.com>
2020-05-03 22:48:44 +0530

    first commit

---
+++ include/nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+          <a href="" class="navbar-brand">CWSBook.com</a>
+          <form action="" class="form-inline mx-auto">
+              <input type="search" class="form-control" size="70">
+              <input type="submit" class="btn btn-dark">
+          </form>
+          
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+              <li class="nav-item"><a href="" class="nav-link">Profile</a></li>
+              <li class="nav-item"><a href="" class="nav-link">Logout</a></li>
+          </ul>
+          
+      </div>
+       
+   </nav>
---
+++ profile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CWSBook.com |connect Together</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+   
+   <?php include "include/nav.php"; ?>
+   
+   <div class="container mt-5">
+       <div class="row">
+           <div class="col-lg-3">
+               <div class="card">
+                  <img src="https://via.placeholder.com/250" alt="">
+                   <div class="card-body">
+                      <h2 class="h6 text-uppercase text-center">Manisha Gupta</h2> 
+                   </div>
+               </div>
+           </div>
+           <div class="col-lg-6">
+               <div class="card">
+                   
+                       <form action="">
+                           <textarea name="" id="" cols="30" rows="6" class="form-control" placeholder="write something here"></textarea>
+                       </form>
+                   <div class="card-body">
+                       <input type="file">
+                       <input type="submit" class="btn btn-primary btn-sm float-right">
+                   </div>
+               </div>
+           </div>
+           <div class="col-lg-3">
+               
+           </div>
+       </div>
+   </div>
+    
+      </body>
+</html>
