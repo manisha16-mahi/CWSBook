38baa4ee3b5b67d777d5055567d72f408f2d9279
manisha16-mahi <manishakrigupta16899@gmail.com>
2020-05-03 20:50:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CWSBook.com |connect Together</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a href="index.php" class="navbar-brand"><h5>CWSBook</h5></a>
+        <div class="container">
+            <table class="ml-auto">
+            <tr>
+                <th class="text-white small">Username/Email</th>
+                <th class="text-white small">Password</th>
+            </tr>
+            <tr>
+                <td><input type="text" placeholder="username/email" class="form-control form-control-sm"></td>
+                <td><input type="password" placeholder="password" class="form-control form-control-sm"></td>
+                <td><input type="submit" class="btn btn-info btn-sm"></td>
+            </tr>
+        </table>
+        </div>
+    </nav>
+    
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-lg-8">
+                <h1 class="display-4">Welcome in NO.1 Social Networking Site</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, quas, nihil? Quibusdam velit non, autem praesentium, cum ullam at nesciunt quos labore, magnam vitae mollitia quas molestiae esse nam dignissimos.</p>
+            </div>
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                       <h2 class="blockquote m-0 p-0">Create an Account</h2>
+                       <small class="text-muted">100% free of cost</small>
+                        <form action="" class="mt-2">
+                            <div class="row">
+                                <div class="form-group col">
+                                <label for="fname" class="m-0 p-0 text-muted">First Name</label>
+                                <input type="text" id="fname" name="fname" class="form-control">
+                            </div>
+                            <div class="form-group col">
+                                <label for="lname" class="m-0 p-0 text-muted">Last Name</label>
+                                <input type="text" id="lname" name="lname" class="form-control">
+                            </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="m-0 p-0 text-muted">Email</label>
+                                <input type="email" id="email" name="email" class="form-control">
+                            </div>
+                             <div class="form-group">
+                                <label for="contact" class="m-0 p-0 text-muted">Contact</label>
+                                <input type="number" id="contact" name="contact" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="dateofbirth" class="m-0 p-0 text-muted">Date of Birth</label>
+                                <input type="date" id="dateofbirth" name="dateofbirth" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="m-0 p-0 text-muted">Password</label>
+                                <input type="password" id="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                
+                                <input type="submit" name="create_account" class="btn btn-success btn-block">
+                            </div>
+                            
+                            
+                            
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
